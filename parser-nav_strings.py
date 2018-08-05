9f3e9d11294129f1bc3c72993dfd95b556aa0509
Павел А <inthesochi2014@gmail.com>
2018-08-05 22:09:11 +0400

    NavigableString +possibly more understandable structure of code

---
+++ parser-nav_strings.py
@@ -0,0 +1,12 @@
+# Test with NavigableString (+more rational and (possibly) more understandable code structure).
+from bs4 import BeautifulSoup
+from lxml import html
+
+
+with open("index.html") as fp:
+    soup = BeautifulSoup(fp, "html.parser")
+    tag1 = soup.p.string #Доступ к строке внутри тега p.
+    tag2 = soup.span.string #Доступ к строке внутри тега span.
+    print(tag1,end='\n\n') # Два перевода строки для наглядности вызова результатов.
+    print(tag2)
+
